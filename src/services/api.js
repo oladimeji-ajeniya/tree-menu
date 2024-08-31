31fbc7d5a77167e3d90015a63ed5cf4be9599db4
Oladimeji Ajeniya <oladimejiajeniya@gmail.com>
2024-08-31 12:00:37 +0100

    fix: fixing https url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://ec2-3-94-206-127.compute-1.amazonaws.com/api",
+  baseURL: "https://ec2-3-94-206-127.compute-1.amazonaws.com/api",
 });
 
 export default api;
